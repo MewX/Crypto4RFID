882aa7c5fb6d050cf623bfac513e058c74743b1c
Harry <a1689938@student.adelaide.edu.au>
2017-08-04 21:19:51 +0930

    added demo programe and EPC G2 paper

---
+++ Demo/WIPS5 firmwares/run-once/Release/subdir_vars.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../catchall.c \
+../main.c 
+
+C_DEPS += \
+./catchall.d \
+./main.d 
+
+OBJS += \
+./catchall.obj \
+./main.obj 
+
+OBJS__QUOTED += \
+"catchall.obj" \
+"main.obj" 
+
+C_DEPS__QUOTED += \
+"catchall.d" \
+"main.d" 
+
+C_SRCS__QUOTED += \
+"../catchall.c" \
+"../main.c" 
+
+
