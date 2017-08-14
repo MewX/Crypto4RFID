cbdba5f7eafe3ec77460b7f71e530b22a68b9192
MewX <imewx@qq.com>
2017-08-15 00:49:42 +0930

    Fixed codes type errors, and added one record.

--- Cipher/CcsProject/main.c
+++ Cipher/CcsProject/main.c
@@ -6,22 +6,23 @@
 int main(void)
 {
     // init variables
-    int num_rounds = 64;
-    long v[2] = { 0x22668D08, 0x1C34F92D }; // encrypted texts of 2 words
+    unsigned int num_rounds = 64;
+    unsigned long v[2] = { 0x22668D08, 0x1C34F92D }; // encrypted texts of 2 words
     const long key[4] = { 0x11111111, 0x22222222, 0x33333333, 0x44444444 };
 
-    __asm nop; // starting position
+    asm(" NOP"); // starting position
 
     // starting the algorithm
-    long v0 = v[0], v1 = v[1], delta = 0x9E3779B9, sum = delta*num_rounds;
-    for (int i = 0; i < num_rounds; i++) {
+    unsigned int i = 0;
+    unsigned long v0 = v[0], v1 = v[1], delta = 0x9E3779B9, sum = delta*num_rounds;
+    for (i = 0; i < num_rounds; i++) {
         v1 -= (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum>>11) & 3]);
         sum -= delta;
         v0 -= (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
     }
     v[0] = v0; v[1] = v1;
 
-    __asm nop; // ending position
+    asm(" NOP"); // ending position
 
 	return 0;
 }
