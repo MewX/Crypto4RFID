1041b4bd314f650b9bc6c8e30aff9829d384faf3
Harry <a1689938@student.adelaide.edu.au>
2017-08-21 22:22:49 +0930

    restructured software project #1

--- MWSproject/crypto4RFID_software/apps.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class Crypto4RfidSoftwareConfig(AppConfig):
-    name = 'crypto4RFID_software'
--- MWSproject/crypto4RFID_software/forms.py
+++
@@ -1,9 +0,0 @@
-
-from django import forms
-from django.template.context_processors import request
-
-class TestForm(forms.Form):
-    host = forms.CharField(label="Host Address")
-    mode = forms.CharField(label="Modes")
-        
-        
