10a4b9d450a84971cdbf4c20c9aebb4b6cca7327
Harry <a1689938@student.adelaide.edu.au>
2017-08-21 22:02:55 +0930

    initial commit for the development envrionment #1

---
+++ MWSproject/crypto4RFID_software/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class Crypto4RfidSoftwareConfig(AppConfig):
+    name = 'crypto4RFID_software'
