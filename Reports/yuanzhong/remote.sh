94e48a5bb77c194eb9311b986b265f368bdd76f8
MewX <imewx@qq.com>
2017-11-03 16:20:06 +1030

    init report folder

---
+++ Reports/yuanzhong/remote.sh
@@ -0,0 +1,4 @@
+pdflatex -interaction nonstopmode yuanzhong_s2.tex &>/dev/null
+bibtex yuanzhong_s2
+pdflatex -interaction nonstopmode yuanzhong_s2.tex
+pdflatex -interaction nonstopmode yuanzhong_s2.tex
---
+++ Reports/yuanzhong/yuanzhong-imgs/README.md
@@ -0,0 +1,2 @@
+This folder is preserved for putting images.
+
